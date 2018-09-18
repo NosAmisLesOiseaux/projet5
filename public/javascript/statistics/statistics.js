ef0fac69ce4cb4b266c07b28274769445c3585c7
Seymos76 <contact@louisthomas.fr>
2018-09-18 20:35:56 +0200

    fixing

--- public/javascript/statistics/statistics.js
+++ public/javascript/statistics/statistics.js
@@ -3,51 +3,51 @@ $( function() {
 
     $.getJSON('https://road-web.fr/api/datastatistics/'+ currentYear +'/', function( data ) {
 
-            $("button").click(function(e)
-            { 
-                var val = this.dataset.valbutton; 
-                $(this).hide();
+        $("button").click(function(e)
+        {
+            var val = this.dataset.valbutton;
+            $(this).hide();
 
-                $('#statisticsByRegion'+ val+'').pagination({
-                    dataSource: 'https://road-web.fr/api/datastatistics/'+ currentYear +'/',
-                    locator: 'items',
-                    pageSize: 1,
-                    totalNumber: data[0].regions[val].numberOfBirds,
-                    autoHidePrevious: true,
-                    autoHideNext: true,  
-            
-                    callback: function(data, pagination) {
-                        var numberOfBirds = data[0].regions[val].numberOfBirds;
-                        var region = data[0].regions[val];
-                        var currentPage = $('#statisticsByRegion'+ val+'').data('pagination').model.pageNumber;
-                        var pageSize = $('#statisticsByRegion'+ val+'').data('pagination').model.pageSize
-                        var start = (currentPage * pageSize) - pageSize;
-                        var limit = start + pageSize;
-                        var totalPage = Math.ceil(numberOfBirds / pageSize);
+            $('#statisticsByRegion'+ val+'').pagination({
+                dataSource: 'https://road-web.fr/api/datastatistics/'+ currentYear +'/',
+                locator: 'items',
+                pageSize: 1,
+                totalNumber: data[0].regions[val].numberOfBirds,
+                autoHidePrevious: true,
+                autoHideNext: true,
 
-                        $('#data'+ val +'').next().attr('id', 'pagination'+ val +'');
-                        if (totalPage <= 1)
-                        {
-                            $('#pagination'+ val +'').hide();
-                        }
+                callback: function(data, pagination) {
+                    var numberOfBirds = data[0].regions[val].numberOfBirds;
+                    var region = data[0].regions[val];
+                    var currentPage = $('#statisticsByRegion'+ val+'').data('pagination').model.pageNumber;
+                    var pageSize = $('#statisticsByRegion'+ val+'').data('pagination').model.pageSize
+                    var start = (currentPage * pageSize) - pageSize;
+                    var limit = start + pageSize;
+                    var totalPage = Math.ceil(numberOfBirds / pageSize);
 
-                        var birds = "";
-                        if (numberOfBirds > 0)
-                        {   
-                            birds += '<ul>';
-                            for (t = start; t < limit; t++)
-                            {
-                                birds += '<li>';
-                                birds += region.birds[t];
-                                birds += '</li>';
-                            }
-                            birds += '</ul>';
-                        }
-
-                        $('#data'+ val +'').html(birds);
+                    $('#data'+ val +'').next().attr('id', 'pagination'+ val +'');
+                    if (totalPage <= 1)
+                    {
+                        $('#pagination'+ val +'').hide();
                     }
-                });
 
-         })
+                    var birds = "";
+                    if (numberOfBirds > 0)
+                    {
+                        birds += '<ul>';
+                        for (t = start; t < limit; t++)
+                        {
+                            birds += '<li>';
+                            birds += region.birds[t];
+                            birds += '</li>';
+                        }
+                        birds += '</ul>';
+                    }
+
+                    $('#data'+ val +'').html(birds);
+                }
+            });
+
+        })
     });
 });
